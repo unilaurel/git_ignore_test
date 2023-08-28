5ab7be90e694fbd2e3896f925547ec2fcee30f14
unilaurel <unilaurel@outlook.com>
2023-08-29 01:24:28 +0900

    3th

--- Hello2.java
+++ Hello2.java
@@ -12,5 +12,6 @@ import java.sql.SQLOutput;
 public class Hello2 {
     public static void main(String[] args) {
         System.out.println("2nd");
+        System.out.println(3);
     }
 }
--- HelloWorld.java
+++ HelloWorld.java
@@ -12,6 +12,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("1111");
         System.out.println("end");
+        System.out.println("3");
     }}
 
 
