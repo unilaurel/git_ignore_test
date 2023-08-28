58c436d7e10712c7610a9552a80a65e16debeccd
unilaurel <unilaurel@outlook.com>
2023-08-29 01:23:12 +0900

    2th

--- Hello2.java
+++ Hello2.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 /**
  * ClassName: Hello2
  * Package: PACKAGE_NAME
@@ -8,4 +10,7 @@
  * @Version 1.0
  */
 public class Hello2 {
+    public static void main(String[] args) {
+        System.out.println("2nd");
+    }
 }
--- HelloWorld.java
+++ HelloWorld.java
@@ -11,6 +11,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("1111");
+        System.out.println("end");
     }}
 
 
